d127643e1d45ee6433c033b32a67c4f913bd0f17
JuozasVainauskas <vainauskasjuozas@gmail.com>
2021-12-03 00:51:21 +0200

    Create a driver for printing string to the screen

--- src/kernel/headers/printer.h
+++ src/kernel/headers/printer.h
@@ -1,5 +1,5 @@
-#ifndef OS_KMAIN_H
-#define OS_KMAIN_H
+#ifndef OS_WRITER_H
+#define OS_WRITER_H
 
 #define FB_MEMORY_MAPPED_IO 0x000B8000
 
@@ -11,8 +11,8 @@
 #define FB_HIGH_BYTE_COMMAND    14
 #define FB_LOW_BYTE_COMMAND     15
 
+void print(char* output, int position);
 void fb_write_cell(int i, char c, unsigned char bg, unsigned char fg);
 void fb_move_cursor(unsigned short pos);
 
 #endif
-
